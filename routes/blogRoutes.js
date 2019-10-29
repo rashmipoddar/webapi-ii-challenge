338b2aaf813645b744d476277d3a74b90307bbb6
Rashmi <poddar.rashmi@gmail.com>
2019-10-29 18:12:28 -0400

    Added endpoints for deleting and updating a blog post

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -81,9 +81,68 @@ router.get('/:id/comments', (req, res) => {
     })
     .catch(error => {
       // console.log(error);
-      res.status().send({error: 'The comments information could not be retrieved.'});
+      res.status(500).send({error: 'The comments information could not be retrieved.'});
     })
 });
 
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  // console.log(id);
+
+  db.findById(id)
+    .then(response => {
+      // console.log(response);
+      if(response.length === 0) {
+        res.status(404).send({error: 'The post with the specified ID does not exist.'});
+      } else {
+        db.remove(id)
+          .then(deleteResponse => {
+            // console.log(deleteResponse);
+            res.status(200).send(response);
+          })
+          .catch(error => {
+            // console.log(error);
+            res.status(500).send({error: 'The post could not be removed.'});
+          })
+      }
+    })
+    .catch(error => {
+      // console.log(error);
+      res.status(500).send({error: 'The post could not be removed.'});
+    })
+});
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  // console.log(id);
+  const updatedBlog = req.body;
+  console.log(updatedBlog);
+
+  db.findById(id)
+    .then(response => {
+      // console.log(response);
+      if(response.length === 0) {
+        res.status(404).send({error: 'The post with the specified ID does not exist.'});
+      } else {
+        if (!req.body.title || !req.body.contents) {
+          res.status(400).send({error: 'Please provide title and contents for the post.'});
+        } else {
+          db.update(id, updatedBlog)
+          .then(updateResponse => {
+            console.log(updateResponse);
+            res.status(200).send(updatedBlog);
+          })
+          .catch(error => {
+            console.log(error);
+            res.status(500).send({error: 'The post information could not be modified.'});
+          })
+        }
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send({error: 'The post information could not be modified.'});
+    })
+})
 
 module.exports = router;
